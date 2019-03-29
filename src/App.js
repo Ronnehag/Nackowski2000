42356205dd88bdd0f6315704c4effaef18c16110
lovehaglund <lovehaglund@live.se>
2019-03-29 10:53:23 +0100

    Auction Details test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import AuctionDetails from './components/Auctions/AuctionDetails'
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-
+        <AuctionDetails/>
       </div>
     );
   }
