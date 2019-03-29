c326e012ecd67b8b18873f63ee6ad4c80dc37a4c
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-03-29 11:05:14 +0100

    merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import AuctionDetails from './components/Auctions/AuctionDetails'
-=======
 import { connect } from 'react-redux';
 import { fetchAuctions } from './store/actions/auctionAction';
 
->>>>>>> 3544eae4f2ca180ac9a3b452f31395f675105691
 class App extends Component {
 
   componentDidMount() {
@@ -17,11 +14,8 @@ class App extends Component {
     console.log(this.props.auctions);
     return (
       <div className="App">
-<<<<<<< HEAD
         <AuctionDetails/>
-=======
       <h3>Hello</h3>
->>>>>>> 3544eae4f2ca180ac9a3b452f31395f675105691
       </div>
     );
   }
