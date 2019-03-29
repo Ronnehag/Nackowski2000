5c2b6567936ee071b9d90dd6db7e4c385e77507c
Sandra Dahlberg <sandrakdahlberg@gmail.com>
2019-03-29 11:34:26 +0100

    La till formulär för att skapa auktion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import CreateNewAuction from './components/Auctions/CreateNewAuction';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+    <CreateNewAuction/>
       </div>
     );
   }
