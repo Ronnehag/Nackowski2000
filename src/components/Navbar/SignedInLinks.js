7a3449ed5e45162d2ecebced7da031bb3442ea61
lovehaglund <lovehaglund@live.se>
2019-04-02 11:53:09 +0200

    fixed nav

--- src/components/Navbar/SignedInLinks.js
+++ src/components/Navbar/SignedInLinks.js
@@ -6,19 +6,21 @@ export class SignedInLinks extends Component {
     render() {
         const { username } = this.props;
         return (
+
             <div>
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <span className="navbar-text">{username}</span>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/newauction" className="nav-link">Ny auktion</Link>
-                    </li>
-                </ul>
                 <div className="form-inline">
-                    <button className="btn-sm btn-custom">Logga Ut</button>
+                    <div className="input-group">
+                        <span className="navbar-text">{username}</span>
+                    </div>
+                    <div className="input-group">
+                        <Link to="/newauction" className="nav-link">Ny auktion</Link>
+                    </div>
+                        <div className="input-group">
+                            <button className="btn-sm btn-custom ml-2">Logga ut</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
+
         )
     }
 }
