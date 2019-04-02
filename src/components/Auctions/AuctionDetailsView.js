7b5fd5fcf2826d21f11c9df66d1a89f34089f50a
lovehaglund <lovehaglund@live.se>
2019-04-02 12:54:42 +0200

    Added dates to detailview

--- src/components/Auctions/AuctionDetailsView.js
+++ src/components/Auctions/AuctionDetailsView.js
@@ -2,11 +2,31 @@ import React from 'react';
 
 export default class AuctionDetailsView extends React.Component {
     render() {
+        var date = new Date(this.props.item.StartDatum.replace('T', ' '));
+        const dateOptions = {
+            hour12: false,
+            timeZone: "UTC",
+            //year: "numeric",
+            day: "numeric",
+            month: "numeric",
+            weekday: "short",
+            hour: "2-digit",
+            minute: "2-digit"
+        }
         return (
             <div>
                 <h2>Produkt</h2>
                 <div className="card">
-                    <div className="card-header">{this.props.item.Titel}</div>
+                    <div className="card-header">
+                    <div className="row">
+                        <div className="col-6">
+                            <div className="text-left">{this.props.item.Titel}</div>
+                        </div>
+                        <div className="col-6">
+                            <div className="text-right">{date.toLocaleString("sv-SE", dateOptions)}</div>
+                        </div>
+                    </div>
+                    </div>
                     <div className="card-body">
                         {this.props.item.Beskrivning}
                     </div>
