4239374b5ba5c992370a53176e69cd5c9fca3f75
Sandra Dahlberg <sandrakdahlberg@gmail.com>
2019-04-03 10:43:24 +0200

    Lagt till

---
+++ src/components/Auctions/SearchList.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import Auction from './Auction';
+
+class SearchList extends Component {
+
+    sortedList = () => {
+        if (this.props.filteredList.length) {
+            return this.props.filteredList.sort((a, b) => {
+                if (a.SlutDatum === b.SlutDatum) return 0;
+                return a.SlutDatum > b.SlutDatum ? 1 : -1;
+            })
+        }
+        else{
+            return [];
+        }
+    }  
+
+    render() {
+
+        let filteredList = this.sortedList();        
+
+        const auctionList = filteredList.length > 0 ? filteredList.map((auction) => {
+            console.log(auction.Bud);
+            return (
+                <Auction item={auction} key={auction.AuktionID} />
+            )
+        }) : (
+                <div className="col-6 offset-6 mt-5 mt-10">
+                    <div className="spinner-grow text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
+            )
+
+        return (
+            <div className="row">
+                {auctionList}
+            </div>
+        )
+    }
+}
+const mapStateToProps = state => {
+    return {
+        filteredList: state.auctions.filteredList
+    }
+}
+export default connect(mapStateToProps)(SearchList);
