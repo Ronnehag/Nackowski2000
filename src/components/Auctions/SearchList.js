50ba69e43e69d28b46d1674d9f47c1fb9d9f4d06
Sandra Dahlberg <sandrakdahlberg@gmail.com>
2019-04-03 10:47:42 +0200

    test

--- src/components/Auctions/SearchList.js
+++ src/components/Auctions/SearchList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import moment from 'moment';
 import Auction from './Auction';
 
 class SearchList extends Component {
