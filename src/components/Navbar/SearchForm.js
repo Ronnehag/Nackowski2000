e48799a183d18e3d4504e02b237e360beb5deeb0
Sandra Dahlberg <sandrakdahlberg@gmail.com>
2019-04-03 11:23:52 +0200

    Sökgrejen

--- src/components/Navbar/SearchForm.js
+++ src/components/Navbar/SearchForm.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { searchAuction } from '../../store/actions/auctionAction';
+import { Redirect } from 'react-router-dom'
 
-class SearchForm extends Component {
+class SearchForm extends React.Component {
 
     state = {
-        searchText: ''
+        searchText: '',
+        redirect: false
     }
 
     handleChange = (event) => {
@@ -18,9 +20,17 @@ class SearchForm extends Component {
         event.preventDefault();
         this.props.dispatch(searchAuction(this.state.searchText)
         )
+        this.setState({ redirect: true });
     }
 
     render() {
+
+        const { redirect } = this.state.redirect;
+
+        if (redirect) {
+            return <Redirect to="/search" />;
+        }
+        
         return (<div className="ml-2">
             <form onSubmit={this.handleSubmit} className="form-inline">
                 <div className="input-group">
