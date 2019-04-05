b4ca399c5c947dcee1ddace6bb75a77f7d5e4a2c
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-04-05 11:37:49 +0200

    fixat så sökformen resettas efter submit

--- src/components/Navbar/SearchForm.js
+++ src/components/Navbar/SearchForm.js
@@ -5,10 +5,13 @@ import { withRouter } from 'react-router-dom'
 
 class SearchForm extends React.Component {
 
-    state = {
-        searchText: '',
-        redirect: false
+    static initialState = () => {
+        return {
+            searchText: '',
+        }
     }
+
+    state = SearchForm.initialState();
 
     handleChange = (event) => {
         this.setState({
@@ -18,16 +21,18 @@ class SearchForm extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.props.dispatch(searchAuction(this.state.searchText))
-        this.setState({ redirect: true });
-        this.props.history.push("/search");
+        this.props.dispatch(searchAuction(this.state.searchText));
+        this.setState(SearchForm.initialState(), () => {
+            this.props.history.push("/search");
+        });
+
     }
 
     render() {
         return (<div className="ml-2">
             <form onSubmit={this.handleSubmit} className="form-inline">
                 <div className="input-group">
-                    <input type="text" onChange={this.handleChange} name="searchText" id="searchText" className="form-control-sm" placeholder="Sök auktioner" />
+                    <input type="text" onChange={this.handleChange} value={this.state.searchText} name="searchText" id="searchText" className="form-control-sm" placeholder="Sök auktioner" />
                 </div>
                 <div className="input-group">
                     <button type="submit" className="btn-sm btn-light ml-2">Sök</button>
